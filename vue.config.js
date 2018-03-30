56566059bdd26e121050c4c3b64ff39f057c99de
Matus <peciar.matus@gmail.com>
2018-03-30 17:45:46 +0800

    webpack config edit and new build

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,8 @@ module.exports = {
   lintOnSave: true,
   configureWebpack: {
     output: {
-      path: __dirname + '/docs'
+      path: __dirname + '/docs',
+      publicPath: './'
     },
     // Merged into the final Webpack config
     plugins: [
